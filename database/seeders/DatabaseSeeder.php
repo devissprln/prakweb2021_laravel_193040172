6a90df0d9c44e0f044efcf66016185b1ece6e4bd
devissprln <devi.ale7@gmail.com>
2021-10-28 01:03:07 +0700

    mengunakan fitur factory dan faker #10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'Devis Suparlan',
-            'email' => 'devissuparlan@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Devis Suparlan',
+        //     'email' => 'devissuparlan@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Syahna Zahra',
-            'email' => 'syahnazhra@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Syahna Zahra',
+        //     'email' => 'syahnazhra@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -40,59 +41,61 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
-                sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
-                perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
-                expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
-                n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
-                perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
-                sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
-                perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
-                expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
-                n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
-                perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-Tiga',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
-                sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
-                perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
-                expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
-                n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
-                perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
+        //         sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
+        //         perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
+        //         expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
+        //         n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
+        //         perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-Empat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
-                sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
-                perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
-                expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
-                n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
-                perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
+        //         sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
+        //         perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
+        //         expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
+        //         n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
+        //         perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-Tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
+        //         sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
+        //         perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
+        //         expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
+        //         n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
+        //         perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-Empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sapiente ullam adipisci commodi
+        //         sint repudiandae rerum quasi dolores, officiis, aliquid quisquam aliquam provident vel harum. Ducimus obcaecati vel 
+        //         perspiciatis accusamus natus debitis unde veniam magni amet repellat. Eos ducimus eaque pariatur at labore blanditiis optio 
+        //         expedita dolorem itaque maiores velit voluptate, incidunt quae, quod cupiditate temporibus exercitationem, voluptates vitae totam.
+        //         n corporis vel adipisci soluta facilis cumque qui animi optio ab dignissimos quidem ipsa doloribus accusamus repellendus, quasi ullam,
+        //         perspiciatis sint vero quas molestias. Cum, ducimus dolore suscipit cupiditate magnam reprehenderit enim dolorem omnis debitis,',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -50,5 +51,12 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+Route::get('/author/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $author->posts,
     ]);
 });
